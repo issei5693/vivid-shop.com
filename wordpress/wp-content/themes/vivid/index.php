9258e8a0f19aac6b6892e5a270da3fbe1f898528
issei tamashiro <issei5693@gmail.com>
2019-02-26 15:56:09 +0900

    index おすすめ商品リンクの修正

--- wordpress/wp-content/themes/vivid/index.php
+++ wordpress/wp-content/themes/vivid/index.php
@@ -59,7 +59,7 @@
                             
                                 <li class="swiper-slide p-recommend-list__item">
                                     <div class="c-card">
-                                        <a class="c-card__link">
+                                        <a class="c-card__link" herf="<?php the_title(); ?>">
 
                                             <?php if (has_post_thumbnail()): ?>
                                                 <figure class="c-card__image">
