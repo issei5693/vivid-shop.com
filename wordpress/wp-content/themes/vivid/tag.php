16e76b81c3f8f8d77f491d3ce906e3cdc485edad
tamashiro <issei5693@gmail.com>
2019-07-04 00:18:15 +0900

    タグページのアーカイブ修正

--- wordpress/wp-content/themes/vivid/tag.php
+++ wordpress/wp-content/themes/vivid/tag.php
@@ -26,48 +26,50 @@
                 <h1 class="c-icon-header">「<?php echo $tag->name; ?>」のカテゴリの商品一覧&nbsp;<?php echo get_display_post_number($the_query); ?>件を表示中&nbsp;(全<?php echo $the_query->found_posts; ?>件)</h1>
 
                 <ul class="p-item-list">
-                <?php
-                    if ($the_query->have_posts()) :
-                    while ($the_query->have_posts()) : $the_query->the_post(); ?>
-                        
-                    <li class="p-item-list__item">
-                        <div class="c-lisence-card">
-                            <a class="c-lisence-card__link" href="<?php the_permalink(); ?>">
-                                 <?php
-                                    if (has_post_thumbnail()): ?>
-                                        <figure class="c-lisence-card__image">
-                                            <img class="c-lisence-card__img" src="<?php the_post_thumbnail_url( 'full' ); ?>" alt="<?php the_title(); ?>">
-                                        </figure>
-                                <?php else: ?>
-                                        <figure class="c-lisence-card__image">
-                                            <img class="c-lisence-card__img" src="<?php echo get_template_directory_uri(); ?>/img/ni_item-thumbnail.png" alt="<?php the_title(); ?>">    
-                                        </figure>
-                                <?php endif;  ?>
+                    <?php
+                        if ($the_query->have_posts()) :
+                        while ($the_query->have_posts()) : $the_query->the_post(); ?>
                             
-                                <h3 class="c-lisence-card__title">
-                                    <span class="c-lisence-card__section"><?php echo get_the_category($id)[0]->name; ?></span>
-                                    <span class="c-lisence-card__section"><?php the_title(); ?></span>
-                                </h3>
-                                <p class="c-lisence-card__content">
-                                    <span class="c-lisence-card__section">30% OFF</span>
-                                    <span class="c-lisence-card__price">¥10,000円</span>
-                                    <s>¥15,000円</s>
-                                </p>
-                            </a>
-                        </div>
-                    </li>
+                            <li class="archive-cart-js-item p-item-list__item">
+                                <div class="c-lisence-card">
+                                    <a class="c-lisence-card__link" href="<?php the_permalink(); ?>">
+                                        <?php
+                                            if (has_post_thumbnail()): ?>
+                                                <figure class="c-lisence-card__image">
+                                                    <img class="c-lisence-card__img acji-item-img" src="<?php the_post_thumbnail_url( 'full' ); ?>" alt="<?php the_title(); ?>">
+                                                </figure>
+                                        <?php else: ?>
+                                                <figure class="c-lisence-card__image">
+                                                    <img class="c-lisence-card__img acji-item-img" src="<?php echo get_stylesheet_directory_uri(); ?>/img/ni_item-thumbnail.png" alt="<?php the_title(); ?>">    
+                                                </figure>
+                                        <?php endif;  ?>
+                                    
+                                        <h3 class="c-lisence-card__title">
+                                            <span class="c-lisence-card__section"><?php echo get_the_category($id)[0]->name; ?></span>
+                                            <span class="c-lisence-card__section acji-item-name"><?php the_title(); ?></span>
+                                        </h3>
+                                        <p class="c-lisence-card__content">
+                                            <s class="c-lisence-card__section"><?php echo number_format(get_field('list_price')); ?>円</s>
+                                            <span class="acji-item-off"></span>
+                                            <span class="c-lisence-card__price acji-item-price"></span>
+                                        </p>
+                                    </a>
+                                </div>
+                                <?php echo get_color_me_shop_item_archive(get_field('item_id')); ?>
+                            </li>
+
+                            <?php
+                                endwhile;
+                                else:
+                            ?>
+                            <li class="p-item-list__item">商品はありません</li>
+                    <?php endif; ?>
+                </ul>
 
                 <?php
-                    endwhile;
-                    else:
+                    echo get_wp_query_pagenation($the_query);
+                    wp_reset_postdata();
                 ?>
-                    <li class="p-item-list__item">商品はありません</li>
-                <?php endif; ?>
-            </ul>
-            <?php
-                echo get_wp_query_pagenation($the_query);
-                wp_reset_postdata();
-            ?>
         </section>
 
     </div>
